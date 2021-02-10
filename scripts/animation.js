7f178fed2e184a561c2e189306a34156c76b9d0d
Alessandro Pedrocco <alessandro.pedrocco@gmail.com>
2021-02-10 17:01:23 +0100

    basic star rotation, zooming and added figcaption

--- scripts/animation.js
+++ scripts/animation.js
@@ -1,4 +1,24 @@
 //problem with module path
+import { Linear, Power4, Power3, TweenMax } from "../node_modules/gsap/gsap-core.js"
 import gsap from "../node_modules/gsap/index.js";
 
-gsap.to('#star',{x:100});
+document.addEventListener('DOMContentLoaded', function domLoaded() {
+    const starAnimation = TweenMax.fromTo(
+        '#star',
+        0.5,
+        {
+            scale: 0.1, 
+            opacity: 0,
+            rotation: -180
+        }, 
+        {
+            scale: 1, 
+            opacity: 1,
+            rotation: 0,
+            ease: Power3,
+            repeat:-1,
+            repeatDelay: 1.5
+        }
+    );
+    
+});
