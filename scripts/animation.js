d21575d5a8bac3bbccb97828030d1c68e69b0269
Alessandro Pedrocco <alessandro.pedrocco@gmail.com>
2021-02-17 22:12:13 +0100

    introduced second svg animation logic

--- scripts/animation.js
+++ scripts/animation.js
@@ -1,4 +1,4 @@
-import { Linear, Power3, TweenMax, Expo, Power4 } from "../node_modules/gsap/gsap-core.js";
+import { Linear, Power3, TweenMax, Expo, Power4, Power2 } from "../node_modules/gsap/gsap-core.js";
 import {gsap} from "../node_modules/gsap/index.js";
 import {
     SIDEBAR, 
@@ -98,6 +98,11 @@ document.addEventListener('DOMContentLoaded', function domLoaded() {
                     title: 'Let\'s fly away'
                 };
                 break;
+            case ('sea'):
+                return {
+                    func: animateSea,
+                    title: 'Sandy chills'
+                }
             default:
                 return 'function() {}';
         }
@@ -116,8 +121,133 @@ document.addEventListener('DOMContentLoaded', function domLoaded() {
 
     SIDEBAR_GETBACK.addEventListener('click', function closeSidebar() {
         setSidebar('off');
-    })
+    });
+
+    function animateSea() {
+
+        let umbrellaArmTimeline = gsap.timeline({repeat: -1, repeatDelay: 2});
+        let skyTimeline = gsap.timeline({repeat: -1});
+
+        gsap.set(
+            '#sea_waves',
+            {
+                x: -300
+            }
+        );
+
+        gsap.set(
+            '#sea_arm',
+            {
+                x: 150,
+                y: 400
+            }
+        );
+
+        gsap.set(
+            '#sea_sun',
+            {
+                x: -1100
+            }
+        );
+
+        skyTimeline.to(
+            '#sea_sky',
+            10,
+            {
+                fill: '#87cefa',
+                delay: 4
+            }
+        )
+        .to(
+            '#sea_sky',
+            16,
+            {
+                fill: '#264a80',
+                delay: 36
+            }
+        )
+        .to(
+            '#sea_sky',
+            10,
+            {
+                fill: '#d7e8fd',
+                delay: 4
+            }
+        );
+        
+        gsap.to(
+            '#sea_sun',
+            80,
+            {
+                x:1400,
+                ease: Linear,
+                repeat: -1
+            }
+        );
+
+        gsap.to(
+            '#sea_waves',
+            1.5,
+            {
+                x:-250,
+                repeat: -1,
+                yoyo: true,
+                ease: Power2.easeIn
+            }
+        );
+
+        gsap.fromTo(
+            '#sea_dusk',
+            16,
+            {
+                opacity: 0,
+                fill: '#000000'
+            },
+            {
+                opacity: 0.5,
+                repeat: -1,
+                delay: 50,
+                yoyo: true,
+                repeatDelay: 50
+            }
+        );
+
+        umbrellaArmTimeline.to(
+            '#sea_umbrella',
+            1,
+            {
+                rotation: -18,
+                transformOrigin: '50%',
+                delay: 1.6,
+                ease: Power2.easeIn
+            }
+        )
+        .to(
+            '#sea_arm',
+            0.6, 
+            {
+                y:300,
+                yoyo:true
+            }
+        )
+        .to(
+            '#sea_umbrella',
+            1,
+            {
+                rotation: 0,
+                transformOrigin: '50%'
+            }
+        )
+        .to(
+            '#sea_arm',
+            1,
+            {
+                y:400
+            }
+        );
+    }
 
     animateLab();
+    animateSea();
     
 });
